4c7aa923feb57d3f6aad06f33b3d6a5bbe8b034f
AbbuShaik123 <sheikhabbu66@gmail.com>
2025-11-06 17:42:44 +0530

    Fix Socket.io CORS/env issue for Render deployment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config(); // <- make sure this is at the very top
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -7,27 +10,27 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // works locally and on Render
+    credentials: true,
   },
 });
+
+const userSocketMap = {};
 
 export function getReceiverSocketID(userId) {
   return userSocketMap[userId];
 }
 
-const userSocketMap = {};
-
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-
   if (userId) userSocketMap[userId] = socket.id;
 
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user  disconnected", socket.id);
+    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
